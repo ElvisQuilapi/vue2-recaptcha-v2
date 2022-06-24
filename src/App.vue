7177f5d1c0ae6e7172829e11cfd9bcb3107b9a13
Wentru <elvisquilapi@gmail.com>
2022-06-23 21:37:28 -0400

    example ok!
    example ready!

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="text-center my-5">
+        <img alt="Vue logo" src="./assets/logo.png" />
+      </div>
+      <form class="row g-3" @submit.prevent="handleSubmit">
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">Email address</label>
+          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlTextarea1" class="form-label">Comment</label>
+          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+        <vue-recaptcha ref="recaptcha" :sitekey="APP_SITE_SECRET" @verify="verifyMethod" @expired="expiredMethod"
+          @render="renderMethod" @error="errorMethod" />
+        <div class="col-auto">
+          <button type="submit" class="btn btn-primary mb-3" :disabled="!disabled">Confirm identity</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { VueRecaptcha } from 'vue-recaptcha';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      disabled: false,
+      APP_SITE_SECRET: ''
+    }
+  },
+  methods: {
+    handleSubmit() {
+      console.log('send request GET/POST')
+    },
+    verifyMethod() {
+
+      this.disabled = true
+    },
+    expiredMethod() {
+      console.log('expiredMethod')
+    },
+    renderMethod() {
+      console.log('renderMethod')
+    },
+    errorMethod() {
+      console.log('errorMethod')
+    },
+
+  },
   components: {
-    HelloWorld
+    VueRecaptcha
+  },
+  created() {
+    this.APP_SITE_SECRET = process.env.VUE_APP_RECAPTCHAV2_SITEKEY
   }
 }
 </script>
@@ -21,7 +67,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
